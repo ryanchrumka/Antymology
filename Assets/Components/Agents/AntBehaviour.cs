c5f351ab3c0775cb14ba6256d9893cfacc002080
Ryan <ryan.chrumka@ucalgary.ca>
2024-02-13 18:18:30 -0700

    Ants moving to other mulch block
    (Finally) created the ability for ants to move on top of nearby mulch blocks that are within a height of 2

--- Assets/Components/Agents/AntBehaviour.cs
+++ Assets/Components/Agents/AntBehaviour.cs
@@ -9,7 +9,8 @@ public class AntBehaviour : MonoBehaviour
     public int currentHealth; // The current health of the ant.
     public int healthDeclineRate = 1; // The rate at which health declines.
     public int mulchHealthAmount = 3; //The amount of health for each mulch block
-    private float healthDeclineAccumulator = 0f;
+    public float healthDeclineAccumulator = 0f;
+    public int scanRadius = 3; // How far the ant can "see" Mulch block
 
 
     // Start is called before the first frame update
@@ -28,6 +29,7 @@ public class AntBehaviour : MonoBehaviour
         healthDeclineAccumulator -= decreaseAmount; // Subtract the applied amount from the accumulator
         currentHealth = Mathf.Max(currentHealth, 0); // Ensure health doesn't go below 0
 
+
         if (currentHealth <= 0)
         {
             Debug.Log("Ant died.");
@@ -37,6 +39,7 @@ public class AntBehaviour : MonoBehaviour
         Vector3Int antPosition = Vector3Int.FloorToInt(transform.position);
         AbstractBlock blockBeneath = WorldManager.Instance.GetBlock(antPosition.x, antPosition.y - 1, antPosition.z);
 
+        //Eat mulch
         if (blockBeneath is MulchBlock)
         {
             Debug.Log("Ate mulch.");
@@ -45,5 +48,25 @@ public class AntBehaviour : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
         }
 
+
+        //Scan for mulch and move there if the mulch exist
+
+
+        Vector3Int antPosition2 = Vector3Int.RoundToInt(transform.position);
+        for (int x = -scanRadius; x <= scanRadius; x++)
+        {
+            for (int y = -2; y <= 2; y++)
+            {
+                for (int z = -scanRadius; z <= scanRadius; z++)
+                {
+
+                    AbstractBlock nearbyBlock = WorldManager.Instance.GetBlock(antPosition.x + x, antPosition.y + y, antPosition.z + z);
+                    if (nearbyBlock is MulchBlock)
+                    {
+                        transform.position = new Vector3(antPosition.x + x,antPosition.y + y + 1, antPosition.z + z);
+                    }
+                }
+            }
+        }
     }
 }
